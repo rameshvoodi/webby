82d7fff883cc8ed42d40591b290fade1b30ec379
rameshvoodi <rameshkumarvoodi2002@gmail.com>
2024-03-21 22:35:56 +0530

    app fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Navbar } from "./components/Navbar";
 import { Component } from "./components/component/component";
-function App({ children }: { children: React.ReactNode }) {
+
+function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {children}
-      {/* <Navbar /> */}
       <Component />
     </ThemeProvider>
   );
